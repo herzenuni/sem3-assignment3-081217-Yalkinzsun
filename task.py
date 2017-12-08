2cbd9a21dcc680108826ba7f3993eef0e5de2835
Yalkinzsun <Kirill9806@gmail.com>
2017-12-08 12:57:13 +0300

    add all

--- task.py
+++ task.py
@@ -6,25 +6,28 @@
 
 # import 
 
-# pprint позволяет в понятном для человека виде форматировать 'сложные' структуры данных 
+# pprint позволяет в понятном для человека виде форматировать 'сложные' структуры данных
+
+import json
 import pprint
 
-filename = ''
+filename = 'data.json'
 
 try:
 
     with open(filename, encoding='utf-8') as data_file:
-        
-        data = #использовать модуль json и метод для считывания данных: (data_file)
+        json_file = open("data.json", "r")
+        cont = json_file.read()
+        data = json.loads(cont)
 
-except ________:
+except FileExistsError:
 
     print("Файл не найден! Файл должен называться: {}".format(filename))
     
     status = 'Файл не найден'
 
 
-pprint(data)
+pprint.pprint(data)
 
 # Вывести в форматированном виде поля: 
 
